1321ed69d81926f98ad131969f58b113db742664
Cassidy Linhares <cassidy.linhares@ontariotechu.net>
2021-02-16 22:17:07 -0500

    add build

---
+++ build/precache-manifest.27fcbd7d929c11d46dace1dfa1f05821.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e63355acc582bcbaf2d9c61088372aed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82d7b8d5d10f37fd1a38",
+    "url": "/static/css/2.3b35224a.chunk.css"
+  },
+  {
+    "revision": "1412ae3e3dd31c90ceb1",
+    "url": "/static/css/main.57a749d6.chunk.css"
+  },
+  {
+    "revision": "82d7b8d5d10f37fd1a38",
+    "url": "/static/js/2.88827b8e.chunk.js"
+  },
+  {
+    "revision": "f9a0aecd075e85693d65b862c01a62f0",
+    "url": "/static/js/2.88827b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1412ae3e3dd31c90ceb1",
+    "url": "/static/js/main.28eeaec0.chunk.js"
+  },
+  {
+    "revision": "6e7d85ad969107dd34f7",
+    "url": "/static/js/runtime-main.54edee9e.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
