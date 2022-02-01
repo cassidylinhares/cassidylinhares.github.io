6778f004322b329382f24afb73bdb81171ede247
Cassidy Linhares <cassidy.linhares@ontariotechu.net>
2022-01-31 23:37:41 -0500

    added projects

--- portfolio/lib/main.dart
+++ portfolio/lib/main.dart
@@ -1,58 +1,136 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:portfolio/header.dart';
+import 'package:portfolio/projects.dart';
 
 void main() {
   runApp(
-    const Portfolio(),
+    MaterialApp(
+      home: const Portfolio(),
+      theme: ThemeData(fontFamily: 'Nunito'),
+      debugShowCheckedModeBanner: false,
+    ),
   );
 }
 
 class Portfolio extends StatelessWidget {
   const Portfolio({Key? key}) : super(key: key);
 
-  Widget _header() {
-    //_ means private
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset('assets/bitmoji.png'),
-          Flexible(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(16, 0, 0, 0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text(
-                    "Hello, I'm Cassidy",
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.w700),
-                    textAlign: TextAlign.left,
-                  ),
-                  Text(
-                    "I'm a software engineer who specializes in full stack and IoT applications.",
-                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.w500),
-                    textAlign: TextAlign.left,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
   Widget _stack() {
     return SizedBox.shrink();
   }
 
   Widget _work() {
-    return SizedBox.shrink();
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: const [
+            Padding(
+              padding: EdgeInsets.fromLTRB(128, 32, 0, 0),
+              child: Text(
+                "Places I've been",
+                textAlign: TextAlign.start,
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.w700),
+              ),
+            ),
+          ],
+        ),
+        Wrap(
+          direction: Axis.horizontal,
+          spacing: 128,
+          crossAxisAlignment: WrapCrossAlignment.center,
+          alignment: WrapAlignment.spaceEvenly,
+          children: [
+            SvgPicture.asset(
+              'assets/companies/cibc.svg',
+              width: 200,
+              fit: BoxFit.fitHeight,
+            ),
+            SvgPicture.asset(
+              'assets/companies/mpac.svg',
+              width: 200,
+              fit: BoxFit.fitHeight,
+            ),
+            SvgPicture.asset(
+              'assets/companies/blackberry.svg',
+              width: 200,
+              fit: BoxFit.fitHeight,
+            ),
+            SvgPicture.asset(
+              'assets/companies/mark43.svg',
+              width: 200,
+              fit: BoxFit.fitHeight,
+            ),
+          ],
+        ),
+      ],
+    );
   }
 
   Widget _projects() {
-    return SizedBox.shrink();
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: const [
+            Padding(
+              padding: EdgeInsets.fromLTRB(128, 32, 0, 0),
+              child: Text(
+                "Things I've made",
+                textAlign: TextAlign.start,
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 64,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    spreadRadius: 5,
+                    blurRadius: 6,
+                    offset: Offset(5, 7),
+                  )
+                ],
+              ),
+              child: Image.asset(
+                'assets/projects/mad_libs.png',
+                width: 600,
+              ),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    spreadRadius: 5,
+                    blurRadius: 6,
+                    offset: Offset(5, 10),
+                  )
+                ],
+              ),
+              child: Image.asset(
+                'assets/projects/plant_pal.png',
+                width: 600,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 
   Widget _footer() {
@@ -61,31 +139,27 @@ class Portfolio extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(fontFamily: 'Nunito'),
-      home: Scaffold(
-        appBar: AppBar(
-          elevation: 1,
-          backgroundColor: Colors.white,
-          title: Text(
-            'Cassidy Linhares',
-            style: TextStyle(color: Colors.black87),
-          ),
-          centerTitle: false,
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        backgroundColor: Colors.white,
+        title: const Text(
+          'Cassidy Linhares',
+          style: TextStyle(color: Colors.black87),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              _header(),
-              _stack(),
-              _work(),
-              _projects(),
-              _footer(),
-            ],
-          ),
+        centerTitle: false,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Header(),
+            _stack(),
+            _work(),
+            const Projects(),
+            _footer(),
+          ],
         ),
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
