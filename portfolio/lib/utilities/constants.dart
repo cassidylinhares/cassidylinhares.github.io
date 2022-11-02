b4365614efb627287ecf2aa36277c876d8fec1dd
Cassidy Linhares <cassidy.linhares@ontariotechu.net>
2022-11-01 22:45:28 -0400

    its responsive

--- portfolio/lib/utilities/constants.dart
+++ portfolio/lib/utilities/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/cupertino.dart';
 
-const kMinWidthOfLargeScreen = 725;
+const kMinWidthOfLargeScreen = 775;
 
 const Color grey100 = Color(0xFFE6E6E6);
--- portfolio/lib/work.dart
+++ portfolio/lib/work.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:portfolio/utilities/constants.dart';
 
 class Work extends StatelessWidget {
   const Work({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    bool isScreenWide =
+        MediaQuery.of(context).size.width >= kMinWidthOfLargeScreen;
+
     return Column(
       children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.start,
-          children: const [
+          children: [
             Padding(
-              padding: EdgeInsets.fromLTRB(128, 32, 0, 0),
-              child: Text(
+              padding: isScreenWide
+                  ? const EdgeInsets.fromLTRB(64, 32, 0, 0)
+                  : const EdgeInsets.fromLTRB(10, 32, 0, 0),
+              child: const Text(
                 "Places I've been",
                 textAlign: TextAlign.start,
                 style: TextStyle(fontSize: 28, fontWeight: FontWeight.w700),
