85169522c56cf899a1b97df5391edb7335c13e4e
Cassidy Linhares <30815527+cassidylinhares@users.noreply.github.com>
2022-11-01 23:36:53 -0400

    Delete portfolio directory

--- portfolio/lib/utilities/constants.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-const kMinWidthOfLargeScreen = 775;
-
-const Color grey100 = Color(0xFFE6E6E6);
--- portfolio/lib/utilities/strings.dart
+++
@@ -1,8 +0,0 @@
-const headerText =
-    "I'm a software engineer who specializes in full stack and IoT applications. Aside from coding, I tend to my indoor garden and cook!";
-
-const iotProjectText =
-    "An IoT Smart water metering system for plants. Never forget to water your plants as the sensor reads from the plant each hour and then you get notified when the moisture drops below the recommended moisture level. You just sign in, to set the plant type and then stick the meter into the plant and leave it.\nDevice created using a NodeMCU & moisture sensor. Currently, only a web application made with React and Django exist.";
-
-const madLibsProjectText =
-    "The mad libs game but on mobile! Created using React-native, players can pick from various stories or quotes and create new random, wacky, and funny stories. Players can read it aloud or have their phone read it back to them and even save it if they like it.\nAuthentication and storage are done with Google's Firebase.";
